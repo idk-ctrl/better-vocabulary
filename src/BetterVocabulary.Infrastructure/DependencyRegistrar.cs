1b3eef77a29dbf0da515edad70f6d2851da7a1f4
Yevhen K <yevhenk@varyence.com>
2020-03-18 12:38:34 +0200

    init project structure

---
+++ src/BetterVocabulary.Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BetterVocabulary.Infrastructure
+{
+    public class DependencyRegistrar
+    {
+        public static IServiceCollection RegisterDependencies(this IServiceCollection services, IConfiguration cfg)
+        {
+            services
+                .AddDbContext<DbContext, DataContext>(options => ConfigureDbContext(cfg, options))
+                .AddTransient(typeof(IRepository<>), typeof(Repository<>))
+                .AddTransient<ICreditUnionDataService, CreditUnionDataService>()
+                .AddTransient<ILocationsProfilesDataProvider, LocationsProfilesDataProvider>()
+                .AddTransient<ICuServicesProviderService, CuServicesProviderService>()
+                .AddTransient(typeof(ICachingService<>), typeof(CachingService<>))
+                .AddSingleton(AutoMapperConfiguration.RegisterMappings().CreateMapper())
+                .AddLogging();
+            return services;
+        }
+    }
+}
